dbd62524f35aa2e3a20707a65f4f7c47053df593
steinuil <steenuil.owl@gmail.com>
2018-11-10 13:36:38 +0100

    refactored generator

--- generate.rkt
+++ generate.rkt
@@ -1,66 +1,10 @@
 #lang racket
 
 (require xml)
+(require markdown/parse)
 
-
-(struct page-info (id url title))
-
-(define (navbar page)
-  (define pages
-    (list (page-info 'blog "/molten-matter/" "Molten Matter")
-          (page-info 'about "/" "About")))
-  `(nav
-    (ul
-     ,@(map (λ (p)
-              `(li (a ([href ,(page-info-url p)]
-                       ,@(if (eq? page (page-info-id p)) '([class "selected"]) '()))
-                      ,(page-info-title p))))
-            pages))))
-
-
-(define (page curr-page title body)
-  `(html
-    (head
-     (title ,title)
-     (meta ([charset "UTF-8"]))
-     (meta ([name "viewport"]
-            [content "width=device-width, initial-scale=1, viewport-fit=cover"]))
-     (link ([rel "stylesheet"]
-            [href "/assets/style.css"]
-            [type "text/css"])))
-    (body ([id ,(string-append (symbol->string curr-page) "-page")])
-          (div
-           ([class "body-container"])
-           (header ,(navbar curr-page))
-           (main ,@body)
-           (footer
-            "this web sight was made with " (a ([href "https://racket-lang.org/"]) "Racket") ".")))))
-
-
-(define about-page
-  (page 'about "steenuil's page"
-        '((div ([class "text"])
-               (p "I welcome you to my humble abode. "
-                  "Make yourself comfortable. Have a link, if you'd like.")
-               (div ([class "table"])
-                    (div "github")  (div (a ([href "https://github.com/steinuil"]) "github.com/steinuil"))
-                    (div "twitter") (div (a ([href "https://twitter.com/steinuil"]) "@steinuil"))
-                    (div "email")   (div (a ([href "mailto:steenuil.owl@gmail.com"]) "steenuil.owl@gmail.com")))
-               (p "I go by the name of " (strong "steenuil") ". "
-                  "You might recognize it as the dutch name of the "
-                  (a ([href "https://en.wikipedia.org/wiki/Athene_noctua"]) "Athene Noctua")
-                  ", also known as the " (em "little owl") " in english.")
-               (p "I write software every now and then, "
-                  "most of which could be classified as "
-                  (a ([href "http://catb.org/jargon/html/Y/yak-shaving.html"]) "yak shaving")
-                  " around small issues I have with the programs I use every day.")
-               (p "I like revisiting old things using new tools, "
-                  "so I often end up spelunking through old, semi-abandoned codebases as research. "
-                  "Sometimes I even try to get them working again.")
-               (p "I currently dwell in " (strong "Italy") ", not too far from the Alps. "
-                  "I speak English and Italian, and I know enough French to get by.")))))
-
-
+;;;
+;;; Blog stuffs
 
 (struct pdate
   (year month day))
@@ -70,9 +14,25 @@
    date
    id
    tags
-   [series #:auto])
-  #:auto-value #f)
+   series
+   unlisted?
+   generate?)
+  #:constructor-name make-blog-post)
 
+(define (post title
+              #:date date
+              #:id id
+              #:tags [tags '()]
+              #:series [series #f]
+              #:unlisted? [unlisted? #f]
+              #:generate? [generate? #t])
+  (make-blog-post title
+                  date
+                  id
+                  tags
+                  series
+                  unlisted?
+                  generate?))
 
 (define (string-pad-left str n x)
   (let ([pad (- n (string-length str))])
@@ -104,78 +64,182 @@
   (pdate>? (blog-post-date p1)
            (blog-post-date p2)))
 
+;;;
+;;; Page stuffs
+
+(struct page-info (id url title))
+
+(define (navbar page page-infos)
+  `(nav
+    (ul
+     ,@(for/list ([pinfo page-infos])
+         `(li (a ([href ,(page-info-url pinfo)]
+                  ,@(if (eq? page (page-info-id pinfo)) '([class "selected"]) '()))
+                 ,(page-info-title pinfo)))))))
+
+(define (page curr-page page-infos title body)
+  `(html
+    (head
+     (title ,title)
+     (meta ([charset "UTF-8"]))
+     (meta ([name "viewport"]
+            [content "width=device-width, initial-scale=1, viewport-fit=cover"]))
+     (link ([rel "stylesheet"]
+            [href "/assets/style.css"]
+            [type "text/css"])))
+    (body ([id ,(string-append (symbol->string curr-page) "-page")])
+          (div
+           ([class "body-container"])
+           (header ,(navbar curr-page page-infos))
+           (main ,@body)
+           (footer
+            "this web sight made with " (a ([href "https://racket-lang.org/"]) "Racket") ".")))))
+
+;;;
+;;; Generation
+
+(define (generate-page url page)
+  (define dir-path (string-append "." url))
+  (unless (directory-exists? dir-path)
+    (make-directory dir-path))
+
+  (call-with-output-file (string-append dir-path "index.html") #:exists 'replace
+    (lambda (out)
+      (write-xml/content
+        (xexpr->xml page)
+        out))))
+
+(define (post-page page-infos post)
+  (define post-body
+    (parse-markdown (file->string (string-append "posts/" (blog-post-id post) ".md"))))
+
+  (page 'blog-post page-infos (blog-post-title post)
+        `((header ,(blog-post-title post))
+          ,@post-body)))
+
+;;;
+;;; Content
+
+(define page-infos
+  (list (page-info 'blog "/molten-matter/" "Molten Matter")
+        (page-info 'about "/" "About")
+        (page-info 'legal "/legal/" "Legal")))
+
+(define about-page
+  (page 'about page-infos "steenuil's page"
+        '((div ([class "text"])
+               (p "I welcome you to my humble abode. "
+                  "Make yourself comfortable. Have a link, if you'd like."))
+          (div ([class "table"])
+               (div "github")  (div (a ([href "https://github.com/steinuil"]) "github.com/steinuil"))
+               (div "twitter") (div (a ([href "https://twitter.com/steinuil"]) "@steinuil"))
+               (div "email")   (div (a ([href "mailto:steenuil.owl@gmail.com"]) "steenuil.owl@gmail.com")))
+          (div ([class "text"])
+               (p "I go by the name of " (strong "steenuil") ". "
+                  "You might recognize it as the dutch name of the "
+                  (a ([href "https://en.wikipedia.org/wiki/Athene_noctua"]) "Athene Noctua")
+                  ", also known as the " (em "little owl") " in english.")
+               (p "I write software every now and then, "
+                  "most of which could be classified as "
+                  (a ([href "http://catb.org/jargon/html/Y/yak-shaving.html"]) "yak shaving")
+                  " around small issues I have with the programs I use every day.")
+               (p "I like revisiting old things using new tools, "
+                  "so I often end up spelunking through old, semi-abandoned codebases as research. "
+                  "Sometimes I even try to get them working again.")
+               (p "I currently dwell in " (strong "Italy") ", not too far from the Alps. "
+                  "I speak English and Italian, and I know enough French to get by.")))))
+
 
 (define blog-posts
   (list
-   (blog-post "The TTY Protocol"
-              (pdate 2017 2 10)
-              "tty"
-              '(programming))
-   (blog-post "Continuations, Promises, and call/cc"
-              (pdate 2017 10 27)
-              "call-cc"
-              '(programming javascript))
-   (blog-post "I survived Ur/Web"
-              (pdate 2018 1 22)
-              "urweb"
-              '(programming urweb))
-   (blog-post "An introduction to typeclasses"
-              (pdate 2018 02 14)
-              "typeclasses"
-              '(programming urweb plt))))
+   (post "The TTY Protocol"
+         #:date (pdate 2017 2 10)
+         #:id "tty"
+         #:tags '(programming)
+         #:generate? #f)
+   (post "Continuations, Promises, and call/cc"
+         #:date (pdate 2017 10 27)
+         #:id "call-cc"
+         #:tags '(programming javascript)
+         #:generate? #f)
+   (post "The social issues of programming languages"
+         #:date (pdate 2017 10 29)
+         #:id "bikeshed"
+         #:tags '(programming)
+         #:generate? #f
+         #:unlisted? #t)
+   (post "I survived Ur/Web"
+         #:date (pdate 2018 1 22)
+         #:id "urweb"
+         #:tags '(programming urweb)
+         #:generate? #f)
+   (post "What the hell did I do this week, anyway?"
+         #:date (pdate 2018 1 29)
+         #:id "week-001"
+         #:series "Weekly log"
+         #:generate? #f
+         #:unlisted? #t)
+   (post "An introduction to typeclasses"
+         #:date (pdate 2018 02 14)
+         #:id "typeclasses"
+         #:tags '(programming urweb plt)
+         #:generate? #f)
+   (post "Overthinking cash in TypeScript"
+         #:date (pdate 2018 11 09)
+         #:id "overthinking-cash"
+         #:tags '(programming typescript)
+         #:unlisted? #t)))
 
 
 (define blog-index
-  (let ([blog-posts (sort blog-posts blog-post>?)])
-    (page 'blog "Molten Matter"
-          `((div ([class "text"])
-                 (p "This is my blog. I called it "
-                    (strong "Molten Matter")
-                    " because I thought it sounded good. "
-                    "I might dump my thoughts on here every now and then."))
-            (figure (img ([width "700"] [height "394"] [src "/assets/images/greenhouse.jpg"]
-                          [srcset "/assets/images/greenhouse@2x.jpg 2x"]))
-                    (figcaption "the exterior of the greenhouse at the Royal Palace in Wien, Austria"))
-            (div ([class "post-list"])
-                 (ul
-                  ,@(map (λ (p)
-                           `(li ([class "post"])
-                                (a ([class "name"] [href ,(string-append "/molten-matter/" (blog-post-id p))]) ,(blog-post-title p))
-                                (span ([class "date"]) ,(pdate->string (blog-post-date p) "/"))))
-                         blog-posts)))))))
+  (page 'blog page-infos "Molten Matter"
+        `((div ([class "text"])
+               (p "This is my blog. I called it "
+                  (strong "Molten Matter")
+                  " because I thought it sounded good. "
+                  "I might dump my thoughts on here every now and then."))
+          (figure (img ([width "700"] [height "394"] [src "/assets/images/greenhouse.jpg"]
+                                      [srcset "/assets/images/greenhouse@2x.jpg 2x"]))
+                  (figcaption "the exterior of the greenhouse at the Royal Palace in Wien, Austria"))
+          (div ([class "post-list"])
+               (ul
+                ,@(for/list ([p (sort blog-posts blog-post>?)]
+                             #:unless (blog-post-unlisted? p))
+                    `(li ([class "post"])
+                         (a ([class "name"]
+                             [href ,(string-append "/molten-matter/" (blog-post-id p))])
+                            ,(blog-post-title p))
+                         (span ([class "date"]) ,(pdate->string (blog-post-date p) "/")))))))))
+
 
 (define legal-page
-  (page 'legal "the legal stuffs page"
-        `(div ([class "text"]
-               [id "legal"])
-              (header "Legal " (a ([href "#legal"]) "#"))
-              (p "This is a static website. It doesn't store any of your data nor use tracking cookies, scripts or anything of the sort. "
-                 "It doesn't load resources from other websites.")
-              (p "All text and pictures on this website are licensed under "
-                 (a ([href "https://creativecommons.org/licenses/by-sa/4.0/"]) "Creative Commons Attribution-ShareAlike 4.0 International")
-                 " (CC BY-SA 4.0), unless otherwise noted. "
-                 (br)
-                 "The code snippets written by me are licensed under the "
-                 (a ([href "https://unlicense.org/"]) "Unlicense")
-                 ", unless otherwise noted.")
-              (p "This website uses some fonts licensed under the "
-                 (a ([href "http://scripts.sil.org/OFL"]) "SIL Open Font License, version 1.1")
-                 ". These are their copyright notices.")
-              (div ([class "table"])
-                   (div (a ([href "https://www.huertatipografica.com/en/fonts/bitter-ht"]) "Bitter"))
-                   (div "Copyright (c) 2013, Sol Matas (sol@huertatipografica.com.ar), with Reserved Font Names 'Bitter'")
-                   (div (a ([href "http://www.omnibus-type.com/fonts/archivo-black/"]) "Archivo Black"))
-                   (div "Copyright 2017 The Archivo Black Project Authors (https://github.com/Omnibus-Type/ArchivoBlack)")))))
+  (page 'legal page-infos "Legal"
+        `((div ([class "text"])
+               (p "This is a static website. It doesn't store any of your data nor use tracking cookies, scripts or anything of the sort. "
+                  "It doesn't load resources from other websites.")
+               (p "All text and pictures on this website are licensed under "
+                  (a ([href "https://creativecommons.org/licenses/by-sa/4.0/"]) "Creative Commons Attribution-ShareAlike 4.0 International")
+                  " (CC BY-SA 4.0), unless otherwise noted. ")
+               (p "The code snippets written by me are licensed under the "
+                  (a ([href "https://unlicense.org/"]) "Unlicense")
+                  ", unless otherwise noted.")
+               (p "This website uses some fonts licensed under the "
+                  (a ([href "http://scripts.sil.org/OFL"]) "SIL Open Font License, version 1.1")
+                  ". These are their copyright notices."))
+          (div ([class "table"])
+               (div (a ([href "https://www.huertatipografica.com/en/fonts/bitter-ht"]) "Bitter"))
+               (div "Copyright (c) 2013, Sol Matas (sol@huertatipografica.com.ar), with Reserved Font Names 'Bitter'")
+               (div (a ([href "http://www.omnibus-type.com/fonts/archivo-black/"]) "Archivo Black"))
+               (div "Copyright 2017 The Archivo Black Project Authors (https://github.com/Omnibus-Type/ArchivoBlack)")))))
 
 
-(call-with-output-file "index.html" #:exists 'replace
-  (λ (out)
-    (write-xml/content
-     (xexpr->xml about-page)
-     out)))
+;;;
+;;; Generate the pages
 
-(call-with-output-file "molten-matter/index.html" #:exists 'replace
-  (λ (out)
-    (write-xml/content
-     (xexpr->xml blog-index)
-     out)))
+(for ([page (list blog-index about-page legal-page)]
+      [pinfo page-infos])
+  (generate-page (page-info-url pinfo) page))
+
+(for ([post blog-posts]
+      #:when (blog-post-generate? post))
+  (generate-page (string-append "/molten-matter/" (blog-post-id post) "/") (post-page page-infos post)))
